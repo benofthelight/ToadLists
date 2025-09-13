537d3d8567f617b2162b75dab432b780cae4d0d0
benofthelight <ben.delaluz@gmail.com>
2025-09-12 21:13:06 -0500

    Fixed issues with "Mark Complete Task function not working"
    deleted "markCompleted" object in Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+//Represents a singular task
 
 public class Task {
+    private String description; //task description
+    private boolean completed; //states if task is true or false
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[✓] " : "[ ] ") + description;
+    }
+
+    public void markComplete() {
+        this.completed = true;
+    }
+
 }
