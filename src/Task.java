ec230fc44c14ee865dadebdb17ec0e2600fd3ac1
benofthelight <ben.delaluz@gmail.com>
2025-09-12 21:32:27 -0500

    restored task.javas missing code for some reaosn

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+// Task.java
+// Represents a single to-do item
 public class Task {
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public void markComplete() {
+        this.completed = true;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[✓] " : "[ ] ") + description;
+    }
 }
+
